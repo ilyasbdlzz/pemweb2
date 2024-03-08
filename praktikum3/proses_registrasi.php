965c4edccd84e591e2b2070b84323c2ab8626013
ilyasbdlzz <ilyaskenway@gmail.com>
2024-03-08 10:37:12 +0700

    Push Praktikum 3 - Skills

---
+++ praktikum3/proses_registrasi.php
@@ -0,0 +1,12 @@
+<?php
+//Array Numeric
+$domisili =["Jakarta", "Bogor","Depok","Tangerang","Bekasi","Jupiter","Bandung"];
+
+//Array Assosiative
+$program_studi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital"
+];
+
+?>
